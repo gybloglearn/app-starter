ed7b8cf541188c4b429eaf54e1b4ffd229da731a
Gyorfi-Balogh <212434909@logon.ds.ge.com>
2016-11-30 09:01:03 +0100

    Adding startcode gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
+var exec = require('gulp-exec');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('run', ['sync'],function(){
+gulp.task('run', ['startcode', 'sync'],function(){
   gulp.watch('*.*', {}, reload);
+});
+gulp.task('startcode', function(cb){
+  return gulp.src('.').pipe(exec('code .'));
 });
 gulp.task('sync', function() {
   var dir = __dirname.substr(__dirname.lastIndexOf('\\')+1,__dirname.length);
-
-    browserSync.init(null,{
+   browserSync.init(null,{
         proxy: "localhost/" + dir,
         browser: 'default'
     });
