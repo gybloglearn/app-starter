fabdd430e5c4a3c8de5617377ebf5428bee4210b
Albano <502678184@logon.ds.ge.com>
2017-01-23 15:43:18 +0100

    lastversion_today

--- app/components/sm_data/sm_data.ctrl.js
+++ app/components/sm_data/sm_data.ctrl.js
@@ -3,12 +3,14 @@ define([], function () {
   function Controller(smdataService, $cookies, $state, $rootScope, $filter) {
     var vm = this;
     vm.sm_datas = [];
+    vm.darab = [];
     vm.sm = "SM4";
     vm.sheetmakers = ["SM1", "SM2", "SM3", "SM4", "SM5", "SM6", "SM7", "SM8", "SM9"];
     vm.datum = $filter('date')(new Date(), 'yyyy-MM-dd');
     vm.datumszam = vm.datum;
     vm.load = load;
     vm.datszam = beallit;
+    feltolt_zero(vm.darab);
 
     function beallit() {
       vm.szam = new Date(vm.datum);
@@ -17,11 +19,16 @@ define([], function () {
 
     function load() {
       vm.dis = true;
+      feltolt_zero(vm.darab);
       vm.sm_datas = [];
       smdataService.get(vm.datum, vm.sm).then(function (response) {
         vm.sm_datas = response.data;
         vm.dis = false;
-        var nowsm="SM4";
+        var nowsm = vm.sm;
+        for (var i = 0; i < vm.sm_datas.length; i++) {
+          hour_grop(vm.sm_datas[i].Event_type, vm.sm_datas[i].timestamp)
+        }
+        console.log(vm.darab);
         setChart(nowsm);
       });
 
@@ -34,31 +41,31 @@ define([], function () {
       load();
     }
 
-    function setChart(nowsm)
-    {
-      vm.darab=[];
+    function setChart(nowsm) {
       vm.sm = nowsm;
-      feltolt_zero(vm.darab);
       vm.chartconfig = {
-          chart: {
-            type: 'column',
-            width: 900,
-            height: 360
-          },
-          series: [{
-            name: 'Termelt lap',
-            data: vm.darab
-          }],
+        chart: {
+          type: 'column',
+          width: 900,
+          height: 360
+        },
+        title: { text: nowsm + " SOE report" },
+        subtitle: { text: 'Forrás: MES adatbázis' },
+        series: [{
+          name: 'Termelt lap',
+          color: "#00b300",
+          data: vm.darab
+        }],
 
-          xAxis: [
-            { categories: feltolt_hour() },
-          ],
-          yAxis: {
-            title: {
-              text: "Darab"
-            }
-          },
-        };
+        xAxis: [
+          { categories: feltolt_hour() },
+        ],
+        yAxis: {
+          title: {
+            text: "Darab"
+          }
+        },
+      };
     }
 
     function feltolt_hour() {
@@ -72,10 +79,25 @@ define([], function () {
       return szamok;
     }
 
-    function feltolt_zero(darab){
+    function feltolt_zero(nulla)
+    {
       for(var i=0;i<24;i++)
       {
-        darab.push(1);
+        nulla[i]=0;
+      }
+      return nulla;
+    }
+
+    function hour_grop(itemtype, itemtime) {
+      for (var i = 0; i < 18; i++) {
+        if (itemtype == "Sheet Production" && new Date(itemtime).getHours() == i + 6) {
+          vm.darab[i]++
+        }
+      }
+      for (var i = 18; i < 24; i++) {
+        if (itemtype == "Sheet Production" && new Date(itemtime).getHours() == i - 18) {
+          vm.darab[i]++
+        }
       }
     }
 
