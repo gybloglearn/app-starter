a5256fed62c59b64049533df68207de12bd98e9d
Albano <502678184@logon.ds.ge.com>
2017-02-23 12:19:59 +0100

    leallchartok

--- app/components/sm_data/sm_data.ctrl.js
+++ app/components/sm_data/sm_data.ctrl.js
@@ -8,9 +8,9 @@ define([], function () {
     vm.szervezesi_veszteseg = [];
     vm.tervezett_veszteseg = [];
     vm.muszaki_technikai_okok = [];
-    vm.sum_szervezesi=0;
-    vm.sum_tervezett=0;
-    vm.sum_okok=0;
+    vm.sum_szervezesi = 0;
+    vm.sum_tervezett = 0;
+    vm.sum_okok = 0;
     vm.sm = "SM4";
     vm.sheetmakers = ["SM1", "SM2", "SM4", "SM5", "SM6", "SM7", "SM8", "SM9"];
     vm.datum = $filter('date')(new Date(), 'yyyy-MM-dd');
@@ -41,9 +41,9 @@ define([], function () {
       vm.szervezesi_veszteseg = [];
       vm.tervezett_veszteseg = [];
       vm.muszaki_technikai_okok = [];
-      vm.sum_szervezesi=0;
-      vm.sum_tervezett=0;
-      vm.sum_okok=0;
+      vm.sum_szervezesi = 0;
+      vm.sum_tervezett = 0;
+      vm.sum_okok = 0;
       vm.smloading = true;
       vm.dis = true;
       feltolt_zero(vm.darab);
@@ -58,16 +58,18 @@ define([], function () {
         var nowsm = vm.sm;
         for (var i = 0; i < vm.sm_datas.length; i++) {
           hour_grop(vm.sm_datas[i].Event_type, vm.sm_datas[i].timestamp);
-          leall(vm.sm_datas[i].timestamp, vm.sm_datas[i].Event_type, vm.sm_datas[i].Ev_Group, vm.sm_datas[i].Event_time, vm.sm_datas[i].Shift_Name, vm.sm_datas[i].Shift_ID);
+          leall(vm.sm_datas[i].timestamp, vm.sm_datas[i].Event_type, vm.sm_datas[i].Ev_Group, vm.sm_datas[i].Event_time, vm.sm_datas[i].Shift_Name);
         }
-        vm.sum_szervezesi=sumpie(vm.szervezesi_veszteseg);
-        vm.sum_tervezett=sumpie(vm.tervezett_veszteseg);
-        vm.sum_okok=sumpie(vm.muszaki_technikai_okok);
+        vm.sum_szervezesi = sumpie(vm.szervezesi_veszteseg);
+        vm.sum_tervezett = sumpie(vm.tervezett_veszteseg);
+        vm.sum_okok = sumpie(vm.muszaki_technikai_okok);
         console.log(vm.sum_szervezesi);
         console.log(vm.sum_tervezett);
         console.log(vm.sum_okok);
 
         setChart(nowsm);
+        setChartxrange(nowsm);
+        setChartpie(nowsm);
         vm.smloading = false;
       });
 
@@ -81,14 +83,12 @@ define([], function () {
       vm.edate = new Date().getTime();
     }
 
-    function sumpie(tomb)
-    {
-      var sum=0;
-      for(var a=0;a<tomb.length;a++)
-      {
-        sum=sum+tomb[a].tartam;
+    function sumpie(tomb) {
+      var sum = 0;
+      for (var a = 0; a < tomb.length; a++) {
+        sum = sum + tomb[a].tartam;
       }
-      sum=sum/60;
+      sum = sum / 60;
       return sum;
     }
 
@@ -127,6 +127,102 @@ define([], function () {
       };
     }
 
+    function setChartxrange(nowsm) {
+      vm.chartconfig_xrange = {
+        chart: {
+          type: 'xrange',
+          width: 1100,
+          height: 200,
+          zoomType: 'x'
+        },
+       legend: {
+          floating: false,
+          align: 'bottom',
+        },
+        xAxis: { title: { text: 'Idő' }, type: 'datetime', dateTimeLabelFormats: { hour: '%d.<br>%H:%M' }, tickInterval: 3600 * 1000},
+        yAxis: { title: { enabled: false }, min: 0, max: 2, categories: ['Szervezési veszteség', 'Tervezett veszteség', 'Műszaki technikai okok'] },
+        series: [
+          {
+            color: '#cc33ff',
+            name: 'Szervezési veszteség',
+            tooltip: {
+              useHTML: true,
+              headerFormat: '<b style="color:{series.color};font-weight:bold;">Szervezési veszteség:</b><br>',
+              pointFormat: '<span style="color:{series.color};">{point.categ}</span><br><span style="font-size:1.2em">{point.x:%H:%M:%S} - {point.x2:%H:%M:%S}</span><br><b style="font-size:10px">{point.subgroup}</b><br><i style="font-size:10px">{point.comment}</i>'
+            },
+            data: vm.szervezesi_veszteseg,
+            borderRadius: 0,
+            pointWidth: 30,
+            borderColor: '#cc33ff'
+          },
+          {
+            color: '#3366ff',
+            name: 'Tervezett veszteség',
+            tooltip: {
+              useHTML: true,
+              headerFormat: '<b style="color:{series.color};font-weight:bold;">Tervezett veszteség:</b><br>',
+              pointFormat: '<span style="color:{series.color};">{point.categ}</span><br><span style="font-size:1.2em">{point.x:%H:%M:%S} - {point.x2:%H:%M:%S}</span><br><b style="font-size:10px">{point.subgroup}</b><br><i style="font-size:10px">{point.comment}</i>'
+            },
+            data: vm.tervezett_veszteseg,
+            borderRadius: 0,
+            pointWidth: 30,
+            borderColor: '#3366ff'
+          },
+          {
+            color: '#e60000',
+            name: 'Műszaki technikai okok',
+           tooltip: {
+              useHTML: true,
+              headerFormat: '<b style="color:{series.color};font-weight:bold;">Műszaki technikai okok:</b><br>',
+              pointFormat: '<span style="color:{series.color};">{point.categ}</span><br><span style="font-size:1.2em">{point.x:%H:%M:%S} - {point.x2:%H:%M:%S}</span><br><b style="font-size:10px">{point.subgroup}</b><br><i style="font-size:10px">{point.comment}</i>'
+            },
+            data: vm.muszaki_technikai_okok,
+            borderRadius: 0,
+            pointWidth: 30,
+            borderColor: '#e60000'
+          }
+        ]
+      };
+    }
+
+    function setChartpie(nowsm) {
+      vm.sm = nowsm;
+      vm.chartconfig_pie = {
+        chart: {
+          type: 'pie',
+          width: 1100,
+          height: 400
+        },
+        title: { text: "Állásidők eloszlása" },
+        subtitle: { text: "Összes eltelt idő" },
+        plotOptions: {
+          pie: {
+            center: ['50%', '50%'],
+            showInLegend: true
+          }
+        },
+        series: [
+          {
+            data: [{
+              name: 'Szervezesi veszteseg',
+              color: "#cc33ff",
+              y: vm.sum_szervezesi
+            },
+            {
+              name: 'Tervezett veszteseg',
+              color: "#3366ff",
+              y: vm.sum_tervezett
+            },
+            {
+              name: 'Műszaki technikai okok',
+              color: "#e60000",
+              y: vm.sum_okok
+            }]
+          }
+        ],
+      };
+    }
+
     function feltolt_hour() {
       var szamok = [];
       for (var i = 6; i < 24; i++) {
@@ -145,8 +241,8 @@ define([], function () {
       return nulla;
     }
 
-    function hour_grop(itemtype, itemtime) {
-      var szamvaltozo = new Date(itemtime).getHours() * 60 + new Date(itemtime).getMinutes();
+    function hour_grop(itemtype, itemstart) {
+      var szamvaltozo = new Date(itemstart).getHours() * 60 + new Date(itemstart).getMinutes();
       for (var i = 0; i < 18; i++) {
         if (itemtype == "Sheet Production" && (szamvaltozo >= ((i + 5) * 60 + 50)) && szamvaltozo < ((i + 6) * 60 + 50)) {
           vm.darab[i]++
@@ -159,32 +255,61 @@ define([], function () {
       }
     }
 
-    function leall(itemstart, itemtype, itemgroup, itemtime, itemshift, itemid) {
+    function leall(itemstart, itemtype, itemgroup, itemtime, itemshift) {
+      var szamvaltozo = new Date(itemstart).getHours() * 60 + new Date(itemstart).getMinutes();
       if (itemtype == "Downtime" && itemgroup == "Szervezesi veszteseg") {
         vm.szervezesi_veszteseg[l] = {};
-        vm.szervezesi_veszteseg[l].start = itemstart;
+        vm.szervezesi_veszteseg[l].x = itemstart+1000*3600;
         vm.szervezesi_veszteseg[l].tartam = itemtime;
-        vm.szervezesi_veszteseg[l].vege = itemstart + (itemtime * 1000);
+        vm.szervezesi_veszteseg[l].x2 = 1000*3600+itemstart + (itemtime * 1000);
         vm.szervezesi_veszteseg[l].szaknev = itemshift;
-        vm.szervezesi_veszteseg[l].szakid = itemid;
+        vm.szervezesi_veszteseg[l].y = 0.5;
+
+        if (szamvaltozo >= 350 && szamvaltozo < 830) {
+          vm.szervezesi_veszteseg[l].szakszam = 1;
+        }
+        else if (szamvaltozo >= 830 && szamvaltozo < 1310) {
+          vm.szervezesi_veszteseg[l].szakszam = 2;
+        }
+        else {
+          vm.szervezesi_veszteseg[l].szakszam = 3;
+        }
         l++;
       }
       if (itemtype == "Downtime" && itemgroup == "Tervezett veszteseg") {
         vm.tervezett_veszteseg[j] = {};
-        vm.tervezett_veszteseg[j].start = itemstart;
+        vm.tervezett_veszteseg[j].x = itemstart+1000*3600;
         vm.tervezett_veszteseg[j].tartam = itemtime;
-        vm.tervezett_veszteseg[j].vege = itemstart + (itemtime * 1000);
+        vm.tervezett_veszteseg[j].x2 = 1000*3600+itemstart + (itemtime * 1000);
         vm.tervezett_veszteseg[j].szaknev = itemshift;
-        vm.tervezett_veszteseg[j].szakid = itemid;
+        vm.tervezett_veszteseg[j].y = 1.5;
+        if (szamvaltozo >= 350 && szamvaltozo < 830) {
+          vm.tervezett_veszteseg[j].szakszam = 1;
+        }
+        else if (szamvaltozo >= 830 && szamvaltozo < 1310) {
+          vm.tervezett_veszteseg[j].szakszam = 2;
+        }
+        else {
+          vm.tervezett_veszteseg[j].szakszam = 3;
+        }
         j++;
       }
       if (itemtype == "Downtime" && itemgroup == "Muszaki technikai okok") {
         vm.muszaki_technikai_okok[k] = {};
-        vm.muszaki_technikai_okok[k].start = itemstart;
+        vm.muszaki_technikai_okok[k].x = itemstart+1000*3600;
         vm.muszaki_technikai_okok[k].tartam = itemtime;
-        vm.muszaki_technikai_okok[k].vege = itemstart + (itemtime * 1000);
+        vm.muszaki_technikai_okok[k].x2 = 1000*3600+itemstart + (itemtime * 1000);
         vm.muszaki_technikai_okok[k].szaknev = itemshift;
-        vm.muszaki_technikai_okok[k].id= itemid;
+        vm.muszaki_technikai_okok[k].y = 2.5;
+        if (szamvaltozo >= 350 && szamvaltozo < 830) {
+          vm.muszaki_technikai_okok[k].szakszam = 1;
+        }
+        else if (szamvaltozo >= 830 && szamvaltozo < 1310) {
+          vm.muszaki_technikai_okok[k].szakszam = 2;
+        }
+        else {
+          vm.muszaki_technikai_okok[k].szakszam = 3;
+        }
         k++;
       }
     }
