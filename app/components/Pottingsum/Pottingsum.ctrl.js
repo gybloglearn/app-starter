5bd251d8a4d02a3f47773c143f1ea229b30174df
Albano <502678184@logon.ds.ge.com>
2017-05-16 10:59:44 +0200

    chartok

--- app/components/Pottingsum/Pottingsum.ctrl.js
+++ app/components/Pottingsum/Pottingsum.ctrl.js
@@ -4,6 +4,7 @@ define([], function () {
     var vm = this;
     vm.data = [];
     vm.selectdatas = [];
+    vm.selecthour = [];
     vm.mch = "Potting4"
     vm.pottings = ["Potting3", "Potting4"];
     vm.datum = $filter('date')(new Date(), 'yyyy-MM-dd');
@@ -11,68 +12,288 @@ define([], function () {
     vm.startdate = $filter('date')(new Date().getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
     vm.enddate = $filter('date')(new Date().getTime() + (24 * 3600 * 1000), 'yyyy-MM-dd');
     vm.load = load;
-    vm.beallit=beallit;
+    vm.beallit = beallit;
 
-    function beallit(){
-      vm.enddate= $filter('date')(new Date(vm.datum).getTime() + (24 * 3600 * 1000), 'yyyy-MM-dd');
-      vm.startdate= $filter('date')(new Date(vm.datum).getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
-      vm.datumszam=vm.datum;
+    function beallit() {
+      vm.enddate = $filter('date')(new Date(vm.datum).getTime() + (24 * 3600 * 1000), 'yyyy-MM-dd');
+      vm.startdate = $filter('date')(new Date(vm.datum).getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
+      vm.datumszam = vm.datum;
     }
 
     function load() {
       createdatenumber();
 
       vm.dis = true;
+      vm.Pottingloading = true;
       vm.data = [];
       vm.selectdatas = [];
-      for(var i=0;i<3;i++){
-      vm.selectdatas[i] = {}
-      vm.selectdatas[i].SHIFT = $filter('shift')(i+1, vm.datum)+" ";
-      vm.selectdatas[i].GEL_PREP = 0;
-      vm.selectdatas[i].URET_PREP_A = 0;
-      vm.selectdatas[i].POST_URET_A = 0;
-      vm.selectdatas[i].ROT = 0;
-      vm.selectdatas[i].URET_PREP_F = 0;
-      vm.selectdatas[i].POST_URET_F = 0;
+      vm.selecthour = [];
+
+      for (var i = 0; i < 3; i++) {
+        vm.selectdatas[i] = {}
+        vm.selectdatas[i].SHIFT = $filter('shift')(i + 1, vm.datum) + " ";
+        vm.selectdatas[i].GEL_PREP = 0;
+        vm.selectdatas[i].URET_PREP_A = 0;
+        vm.selectdatas[i].POST_URET_A = 0;
+        vm.selectdatas[i].ROT = 0;
+        vm.selectdatas[i].URET_PREP_F = 0;
+        vm.selectdatas[i].POST_URET_F = 0;
+        vm.selectdatas[i].MODULS = [[], [], [], [], [], []];
+      }
+
+      for (var i = 0; i < 24; i++) {
+        vm.selecthour[i] = {}
+        if (i < 19) {
+          vm.selecthour[i].TIME = (i + 5) * 60 + 50;
+        }
+        else {
+          vm.selecthour[i].TIME = (i - 19) * 60 + 50;
+        }
+        vm.selecthour[i].GEL = 0;
+        vm.selecthour[i].URETA = 0;
+        vm.selecthour[i].PURETA = 0;
+        vm.selecthour[i].ROT = 0;
+        vm.selecthour[i].URETF = 0;
+        vm.selecthour[i].PURETF = 0;
       }
 
       SumserviceService.get(vm.startdate, vm.enddate, vm.mch).then(function (response) {
         vm.data = response.data;
         vm.dis = false;
-        for(var i=0;i<vm.data.length;i++){
-          for(var j=0;j<vm.selectdatas.length;j++){
-            if(vm.selectdatas[j].SHIFT==vm.data[i].PT_GEL_PREP_S && (new Date(vm.data[i].PT_GEL_PREP_DT).getTime()<vm.vege) && (new Date(vm.data[i].PT_GEL_PREP_DT).getTime()>=vm.kezdo)){
+        var gelszam1 = 0;
+        var uretalso1 = 0;
+        var uretalsoesztetika1 = 0;
+        var ford1=0;
+        var uretfelso1=0;
+        var uretfelsoesztetika1 = 0;
+
+        for (var i = 0; i < vm.data.length; i++) {
+          for (var j = 0; j < vm.selectdatas.length; j++) {
+            if (vm.selectdatas[j].SHIFT == vm.data[i].PT_GEL_PREP_S && (new Date(vm.data[i].PT_GEL_PREP_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_GEL_PREP_DT).getTime() >= vm.kezdo)) {
               vm.selectdatas[j].GEL_PREP++;
+              vm.selectdatas[j].MODULS[0].push(vm.data[i].JobID);
+              gelszam1++;
             }
-            if(vm.selectdatas[j].SHIFT==vm.data[i].PT_URET_PREP_A_S && (new Date(vm.data[i].PT_URET_PREP_A_DT).getTime()<vm.vege) && (new Date(vm.data[i].PT_URET_PREP_A_DT).getTime()>=vm.kezdo)){
+            if (vm.selectdatas[j].SHIFT == vm.data[i].PT_URET_PREP_A_S && (new Date(vm.data[i].PT_URET_PREP_A_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_URET_PREP_A_DT).getTime() >= vm.kezdo)) {
               vm.selectdatas[j].URET_PREP_A++;
+              vm.selectdatas[j].MODULS[1].push(vm.data[i].JobID);
+              uretalso1++;
             }
-            if(vm.selectdatas[j].SHIFT==vm.data[i].PT_POST_URET_A_S && (new Date(vm.data[i].PT_POST_URET_A_DT).getTime()<vm.vege) && (new Date(vm.data[i].PT_POST_URET_A_DT).getTime()>=vm.kezdo)){
+            if (vm.selectdatas[j].SHIFT == vm.data[i].PT_POST_URET_A_S && (new Date(vm.data[i].PT_POST_URET_A_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_POST_URET_A_DT).getTime() >= vm.kezdo)) {
               vm.selectdatas[j].POST_URET_A++;
+              vm.selectdatas[j].MODULS[2].push(vm.data[i].JobID);
+              uretalsoesztetika1++;
             }
-            if(vm.selectdatas[j].SHIFT==vm.data[i].PT_ROT_S && (new Date(vm.data[i].PT_ROT_DT).getTime()<vm.vege) && (new Date(vm.data[i].PT_ROT_DT).getTime()>=vm.kezdo)){
+            if (vm.selectdatas[j].SHIFT == vm.data[i].PT_ROT_S && (new Date(vm.data[i].PT_ROT_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_ROT_DT).getTime() >= vm.kezdo)) {
               vm.selectdatas[j].ROT++;
+              vm.selectdatas[j].MODULS[3].push(vm.data[i].JobID);
+              ford1++;
             }
-            if(vm.selectdatas[j].SHIFT==vm.data[i].PT_URET_PREP_F_S && (new Date(vm.data[i].PT_URET_PREP_F_DT).getTime()<vm.vege) && (new Date(vm.data[i].PT_URET_PREP_F_DT).getTime()>=vm.kezdo)){
+            if (vm.selectdatas[j].SHIFT == vm.data[i].PT_URET_PREP_F_S && (new Date(vm.data[i].PT_URET_PREP_F_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_URET_PREP_F_DT).getTime() >= vm.kezdo)) {
               vm.selectdatas[j].URET_PREP_F++;
+              vm.selectdatas[j].MODULS[4].push(vm.data[i].JobID);
+              uretfelso1++;
             }
-            if(vm.selectdatas[j].SHIFT==vm.data[i].PT_POST_URET_F_S && (new Date(vm.data[i].PT_POST_URET_F_DT).getTime()<vm.vege) && (new Date(vm.data[i].PT_POST_URET_F_DT).getTime()>=vm.kezdo)){
+            if (vm.selectdatas[j].SHIFT == vm.data[i].PT_POST_URET_F_S && (new Date(vm.data[i].PT_POST_URET_F_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_POST_URET_F_DT).getTime() >= vm.kezdo)) {
               vm.selectdatas[j].POST_URET_F++;
+              vm.selectdatas[j].MODULS[5].push(vm.data[i].JobID);
+              uretfelsoesztetika1++;
             }
           }
         }
-        console.log(vm.selectdatas);
+
+        var gelszam2 = 0;
+        var uretalso2 = 0;
+        var uretalsoesztetika2 = 0;
+        var ford2=0;
+        var uretfelso2=0;
+        var uretfelsoesztetika2 = 0;
+
+        for (var i = 0; i < vm.data.length; i++) {
+          for (var j = 0; j < vm.selecthour.length; j++) {
+            if ((new Date(vm.data[i].PT_GEL_PREP_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_GEL_PREP_DT).getTime() >= vm.kezdo)) {
+              var gelszamvaltozo = new Date(vm.data[i].PT_GEL_PREP_DT).getHours() * 60 + new Date(vm.data[i].PT_GEL_PREP_DT).getMinutes();
+              if (gelszamvaltozo >= vm.selecthour[j].TIME && gelszamvaltozo < vm.selecthour[j].TIME + 60) {
+                vm.selecthour[j].GEL++;
+                gelszam2++;
+              }
+            }
+            if ((new Date(vm.data[i].PT_URET_PREP_A_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_URET_PREP_A_DT).getTime() >= vm.kezdo)) {
+              var uretaszamvaltozo = new Date(vm.data[i].PT_URET_PREP_A_DT).getHours() * 60 + new Date(vm.data[i].PT_URET_PREP_A_DT).getMinutes();
+              if (uretaszamvaltozo >= vm.selecthour[j].TIME && uretaszamvaltozo < vm.selecthour[j].TIME + 60) {
+                vm.selecthour[j].URETA++;
+                uretalso2++
+              }
+            }
+            if ((new Date(vm.data[i].PT_POST_URET_A_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_POST_URET_A_DT).getTime() >= vm.kezdo)) {
+              var puretaszamvaltozo = new Date(vm.data[i].PT_POST_URET_A_DT).getHours() * 60 + new Date(vm.data[i].PT_POST_URET_A_DT).getMinutes();
+              if (puretaszamvaltozo >= vm.selecthour[j].TIME && puretaszamvaltozo < vm.selecthour[j].TIME + 60) {
+                vm.selecthour[j].PURETA++;
+                uretalsoesztetika2++;
+              }
+            }
+            if ((new Date(vm.data[i].PT_ROT_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_ROT_DT).getTime() >= vm.kezdo)) {
+              var rotszamvaltozo = new Date(vm.data[i].PT_ROT_DT).getHours() * 60 + new Date(vm.data[i].PT_ROT_DT).getMinutes();
+              if (rotszamvaltozo >= vm.selecthour[j].TIME && rotszamvaltozo < vm.selecthour[j].TIME + 60) {
+                vm.selecthour[j].ROT++;
+                ford2++;
+              }
+            }
+            if ((new Date(vm.data[i].PT_URET_PREP_F_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_URET_PREP_F_DT).getTime() >= vm.kezdo)) {
+              var uretfszamvaltozo = new Date(vm.data[i].PT_URET_PREP_F_DT).getHours() * 60 + new Date(vm.data[i].PT_URET_PREP_F_DT).getMinutes();
+              if (uretfszamvaltozo >= vm.selecthour[j].TIME && uretfszamvaltozo < vm.selecthour[j].TIME + 60) {
+                vm.selecthour[j].URETF++;
+                uretfelso2++;
+              }
+            }
+            if ((new Date(vm.data[i].PT_POST_URET_F_DT).getTime() < vm.vege) && (new Date(vm.data[i].PT_POST_URET_F_DT).getTime() >= vm.kezdo)) {
+              var puretfszamvaltozo = new Date(vm.data[i].PT_POST_URET_F_DT).getHours() * 60 + new Date(vm.data[i].PT_POST_URET_F_DT).getMinutes();
+              if (puretfszamvaltozo >= vm.selecthour[j].TIME && puretfszamvaltozo < vm.selecthour[j].TIME + 60) {
+                vm.selecthour[j].PURETF++;
+                uretfelsoesztetika2++;
+              }
+            }
+          }
+        }
+        vm.selecthour[18].GEL += (gelszam1 - gelszam2);
+        vm.selecthour[18].URETA += (uretalso1 - uretalso2);
+        vm.selecthour[18].PURETA += (uretalsoesztetika1 - uretalsoesztetika2);
+        vm.selecthour[18].ROT += (ford1 - ford2);
+        vm.selecthour[18].URETF += (uretfelso1 - uretfelso2);
+        vm.selecthour[18].PURETF += (uretfelsoesztetika1 - uretfelsoesztetika2);
+        
+        setChart(vm.mch);
+        vm.Pottingloading = false;
       });
     }
 
-    function createdatenumber(){
-      var year=new Date(vm.datum).getFullYear();
-      var month=new Date(vm.datum).getMonth()+1;
-      var day=new Date(vm.datum).getDate();
-      var kezdo=year+"-0"+month+"-"+day+" 05:50:00";
-      var vege=year+"-0"+month+"-"+(day+1)+" 05:50:00";
-      vm.kezdo=new Date(kezdo).getTime();
-      vm.vege=new Date(vege).getTime();
+    function createdatenumber() {
+      var year = new Date(vm.datum).getFullYear();
+      var month = new Date(vm.datum).getMonth() + 1;
+      var day = new Date(vm.datum).getDate();
+      var kezdo = year + "-0" + month + "-" + day + " 05:50:00";
+      var vege = year + "-0" + month + "-" + (day + 1) + " 05:50:00";
+      vm.kezdo = new Date(kezdo).getTime();
+      vm.vege = new Date(vege).getTime();
+    }
+
+    function setChart(nowpotting) {
+      vm.chartconfig = {
+        chart: {
+          type: 'column'
+        },
+        plotOptions: {
+          column: {
+            stacking: 'normal'
+          }
+        },
+        title: { text: nowpotting + " Report" },
+        series: [
+          {
+            name: 'Felső rész esztétika',
+            color: "#9933ff",
+            data: feltoltpuretf(vm.selecthour),
+            stack: 'Modulok'
+          },
+          {
+            name: 'Uretán felső rész',
+            color: "#660066",
+            data: feltolturetf(vm.selecthour),
+            stack: 'Modulok'
+          },
+          {
+            name: 'Fordítás',
+            color: "#cccc00",
+            data: feltoltrot(vm.selecthour),
+            stack: 'Modulok'
+          },
+          {
+            name: 'Alsó rész esztétika',
+            color: "#cc3300",
+            data: feltoltpureta(vm.selecthour),
+            stack: 'Modulok'
+          },
+          {
+            name: 'Uretán alsó rész',
+            color: "#ff9900",
+            data: feltoltureta(vm.selecthour),
+            stack: 'Modulok'
+          },
+          {
+            name: 'Gél',
+            color: "#33ccff",
+            data: feltoltgel(vm.selecthour),
+            stack: 'Modulok'
+          }
+        ],
+        xAxis: [
+          { categories: feltolt_hour() },
+        ],
+        yAxis: {
+          title: {
+            text: "Modul"
+          }
+        },
+      };
+    }
+
+    function feltoltpuretf(tomb) {
+      var puretf = [];
+      for (var i = 0; i < tomb.length; i++) {
+        puretf.push(tomb[i].PURETF);
+      }
+      return puretf;
+    }
+
+    function feltolturetf(tomb) {
+      var uretf = [];
+      for (var i = 0; i < tomb.length; i++) {
+        uretf.push(tomb[i].URETF);
+      }
+      return uretf;
+    }
+
+    function feltoltrot(tomb) {
+      var rot = [];
+      for (var i = 0; i < tomb.length; i++) {
+        rot.push(tomb[i].ROT);
+      }
+      return rot;
+    }
+
+    function feltoltpureta(tomb) {
+      var pureta = [];
+      for (var i = 0; i < tomb.length; i++) {
+        pureta.push(tomb[i].PURETA);
+      }
+      return pureta;
+    }
+
+    function feltoltureta(tomb) {
+      var ureta = [];
+      for (var i = 0; i < tomb.length; i++) {
+        ureta.push(tomb[i].URETA);
+      }
+      return ureta;
+    }
+
+    function feltoltgel(tomb) {
+      var gel = [];
+      for (var i = 0; i < tomb.length; i++) {
+        gel.push(tomb[i].GEL);
+      }
+      return gel;
+    }
+
+    function feltolt_hour() {
+      var szamok = [];
+      for (var i = 6; i < 24; i++) {
+        szamok.push(i < 10 ? "0" + i : "" + i);
+      }
+      for (var j = 0; j < 6; j++) {
+        szamok.push("0" + j);
+      }
+      return szamok;
     }
 
 
