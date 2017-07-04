55f8c1f50a18eac1dfca82e07d215e28a4a70762
Albano <502678184@logon.ds.ge.com>
2017-07-04 08:52:45 +0200

    +drying3

--- app/components/services/data.service.js
+++ app/components/services/data.service.js
@@ -3,17 +3,25 @@ define([], function () {
 
   function Service($http) {
     var service = {
-      get: get
+      get: get,
+      getdrying: getdrying
     };
 
     return service;
 
     ///////////
 
-    function get(startd,num) {
+    function get(startd, num) {
       var req = {
         method: 'GET',
         url: 'app/components/php/PottingDashboard.php?startdate=' + startd + '&machinename=Potting4&phasename=' + num
+      };
+      return $http(req);
+    }
+    function getdrying() {
+      var req = {
+        method: 'GET',
+        url: 'app/components/php/Drying.php?mch=Drying3'
       };
       return $http(req);
     }
