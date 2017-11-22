c011c3f7a5424b249dc8e62cff062b774e4d8ee5
Gyorfi-Balogh <gyorif.balogh@gmail.com>
2017-11-22 15:29:34 +0100

    Add untracked files

---
+++ app/components/PHP/sapdata.php
@@ -0,0 +1,38 @@
+<?php
+
+  $f = fopen('sapdata/sapdata.csv', 'r') or die ('Nincs meg a file');
+
+  $result = [];
+
+  $result["crdate"] = date('Y-m-d H:i:s', fstat($f)['mtime']);
+  $result["size"] = floatval(fstat($f)['size']/(1024*1024));
+  $result["data"] = [];
+
+  while(!feof($f)){
+    $rdata = fgets($f);
+    $rdata = explode(',', $rdata);
+    array_push($result["data"], $rdata);
+  }
+
+  fclose($f);
+
+  $re = array();
+  $r = $result["data"];
+  foreach($r as $k=>$res){
+    if($k > 0){
+      $row = array();
+      foreach($res as $x=>$y){
+        if($r[0][$x] == 'NAP'){
+          $row[$r[0][$x]] = date('Y-m-d', ($y - 25569)*86400);
+        } else {
+          $row[$r[0][$x]] = intval($y);
+        }
+      }
+      array_push($re, $row);
+    }
+  }
+  array_pop($re);
+  $result["data"] = $re;
+  echo json_encode($result);
+
+?>
---
+++ app/components/directives/mystockchart.js
@@ -0,0 +1,25 @@
+define([], function () {
+    'use strict';
+
+    function mychart() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                config: '='
+            },
+            template: function (elem, attrs) { return '<div id="' + attrs.id + '" style="margin: 20px auto;"></div>' },
+            link: function (scope, element, attrs) {
+                scope.$watch('config', function (newVal, oldVal) {
+                    if (newVal) {
+                        newVal.chart.renderTo = attrs.id;
+                        var ch = new Highcharts.StockChart(newVal);
+                    }
+                });
+            }
+        };
+    };
+
+
+    return mychart;
+});
