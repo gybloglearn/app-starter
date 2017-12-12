ce1d7c8262cdc6e99583d4b0b49257533a5cd691
Albano <502678184@logon.ds.ge.com>
2017-12-12 09:14:19 +0100

    num

--- app/components/start/start.ctrl.js
+++ app/components/start/start.ctrl.js
@@ -209,11 +209,11 @@ define([], function () {
             vm.drynumdata = response.data[i];
           }
         }
-        if (vm.drynumdata.frame <= 21) {
+        if (vm.drynumdata.frame <= 23) {
           vm.usenumber = vm.drynumdata.frame;
         }
         else{
-          vm.usenumber=21;
+          vm.usenumber=23;
         }
         vm.savedate = vm.drynumdata.save;
         vm.todate=$filter('date')(new Date(vm.savedate).getTime()+(6*3600*1000), 'yyyy-MM-dd HH:mm');
