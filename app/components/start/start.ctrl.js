0cbb616bef7a73f3793e011376d3d54c4bb5ae44
root <root@zedashboards.zem.hu>
2018-04-11 08:04:03 +0200

    comment out login line

--- app/components/start/start.ctrl.js
+++ app/components/start/start.ctrl.js
@@ -145,7 +145,7 @@ define([], function () {
     activate();
 
     function activate() {
-      (!$cookies.getObject('user') ? $state.go('login') : $rootScope.user = $cookies.getObject('user'));
+      //(!$cookies.getObject('user') ? $state.go('login') : $rootScope.user = $cookies.getObject('user'));
       loadall();
     }
   }
