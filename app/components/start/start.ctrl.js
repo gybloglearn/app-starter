935be1da67b6432b9381286b5787181ac22e5079
Albano <503045800@logon.ds.ge.com>
2018-04-18 09:44:51 +0200

    refresh

--- app/components/start/start.ctrl.js
+++ app/components/start/start.ctrl.js
@@ -4,6 +4,7 @@ define([], function () {
     var vm = this;
     vm.startdate = $filter('date')(new Date().getTime() - (7 * 24 * 3600 * 1000), 'yyyy-MM-dd');
     vm.enddate = $filter('date')(new Date(), 'yyyy-MM-dd');
+    vm.loading = false;
 
     function loadall() {
       vm.loading = true;
@@ -30,21 +31,26 @@ define([], function () {
         { name: 'SpinLine #236', color: 'rgb(255, 153, 0)', data: [] },
         { name: 'Terv', color: 'rgb(255,0,0)', type: 'line', data: [] }
       ];
+
       var sdn = new Date(vm.startdate).getTime();
       var edn = new Date(vm.enddate).getTime();
       while (sdn <= edn) {
         vm.cats.push($filter('date')(sdn, 'yyyy-MM-dd'));
+        vm.chartData[0].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), y: 0 });
+        vm.chartData[1].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), y: 0 });
         vm.chartData[2].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), y: 110 });
         sdn += (24 * 3600 * 1000);
-
       }
+
       for (var i = 0; i < tb.length; i++) {
-        for (var j = 0; j < vm.chartData.length; j++) {
-          if (j == 0 && tb[i].machine == "SpinLine #136") {
-            vm.chartData[j].data.push({ cat: tb[i].item1, y: tb[i].textbox2 });
-          }
-          else if (j == 1 && tb[i].machine == "SpinLine #236") {
-            vm.chartData[j].data.push({ cat: tb[i].item1, y: tb[i].textbox2 });
+        for (var j = 0; j < vm.chartData.length - 1; j++) {
+          for (var k = 0; k < vm.chartData[j].data.length; k++) {
+            if (j == 0 && tb[i].machine == "SpinLine #136" && tb[i].item1 == vm.chartData[j].data[k].cat) {
+              vm.chartData[j].data[k].y = tb[i].textbox2;
+            }
+            else if (j == 1 && tb[i].machine == "SpinLine #236" && tb[i].item1 == vm.chartData[j].data[k].cat) {
+              vm.chartData[j].data[k].y = tb[i].textbox2;
+            }
           }
         }
       }
@@ -91,11 +97,16 @@ define([], function () {
     }
 
     function create_downtime_chartdata(tb) {
-      console.log(tb);
+
       vm.categ = [];
+      vm.alldata = [
+        { name: 'SpinLine #136', color: 'rgb(51, 204, 51)', data: [] },
+        { name: 'SpinLine #236', color: 'rgb(255, 153, 0)', data: [] }
+      ];
+
       vm.chData = [
-        { name: 'Váratlan Gépleállás', color: 'rgb(255,0,0)', data: [], stack : "SpinLine #136", showInLegend: false },
-        { name: 'Váratlan Gépleállás', color: 'rgb(255,0,0)', data: [], stack : "SpinLine #236" },
+        { name: 'Váratlan Gépleállás', color: 'rgb(255,0,0)', data: [], stack: "SpinLine #136", showInLegend: false },
+        { name: 'Váratlan Gépleállás', color: 'rgb(255,0,0)', data: [], stack: "SpinLine #236" },
         { name: 'Tervezett állás', color: 'rgb(50,100,200)', data: [], stack: "SpinLine #136", showInLegend: false },
         { name: 'Tervezett állás', color: 'rgb(50,100,200)', data: [], stack: "SpinLine #236" },
         { name: 'Fonatoló hiba', color: 'rgb(150,150,150)', data: [], stack: "SpinLine #136", showInLegend: false },
@@ -107,20 +118,34 @@ define([], function () {
       var edn = new Date(vm.enddate).getTime();
       while (sdn <= edn) {
         var ak = $filter('date')(sdn, 'yyyy-MM-dd');
-        //vm.categ.push($filter('date')(sdn, 'yyyy-MM-dd') + "-#136");
         vm.categ.push($filter('date')(sdn, 'yyyy-MM-dd'));
-        vm.chData[0].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Váratlan Gépleállás" }), 'Duration_s_')) });
-        vm.chData[1].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Váratlan Gépleállás" }), 'Duration_s_')) });
-        vm.chData[2].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Tervezett állás" }), 'Duration_s_')) });
-        vm.chData[3].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Tervezett állás" }), 'Duration_s_')) });
-        vm.chData[4].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Fonatoló hiba" }), 'Duration_s_')) });
-        vm.chData[5].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Fonatoló hiba" }), 'Duration_s_')) });
-        vm.chData[6].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Operátor hiba" }), 'Duration_s_')) });
-        vm.chData[7].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Operátor hiba" }), 'Duration_s_')) });
+
+        vm.alldata[0].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), y: 0 });
+        vm.alldata[1].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), y: 0 });
+
+        vm.chData[0].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Váratlan Gépleállás" }), 'Duration_s_'))) / 3600 });
+        vm.chData[1].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Váratlan Gépleállás" }), 'Duration_s_'))) / 3600 });
+        vm.chData[2].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Tervezett állás" }), 'Duration_s_'))) / 3600 });
+        vm.chData[3].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Tervezett állás" }), 'Duration_s_'))) / 3600 });
+        vm.chData[4].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Fonatoló hiba" }), 'Duration_s_'))) / 3600 });
+        vm.chData[5].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Fonatoló hiba" }), 'Duration_s_'))) / 3600 });
+        vm.chData[6].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #136", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL101", startdate: ak, Reason_group_Name: "Operátor hiba" }), 'Duration_s_'))) / 3600 });
+        vm.chData[7].data.push({ cat: $filter('date')(sdn, 'yyyy-MM-dd'), stack: "SpinLine #236", y: (parseFloat($filter('sumField')($filter('filter')(tb, { Machine: "SPL102", startdate: ak, Reason_group_Name: "Operátor hiba" }), 'Duration_s_'))) / 3600 });
         sdn += (24 * 3600 * 1000);
       }
-      console.log(vm.categ);
-      console.log(vm.chData);
+
+      for (var i = 0; i < tb.length; i++) {
+        for (var j = 0; j < vm.alldata.length; j++) {
+          for (var k = 0; k < vm.alldata[j].data.length; k++) {
+            if (j == 0 && tb[i].Machine == "SPL101" && tb[i].startdate == vm.alldata[j].data[k].cat) {
+              vm.alldata[j].data[k].y += tb[i].Duration_s_ / 3600;
+            }
+            else if (j == 1 && tb[i].Machine == "SPL102" && tb[i].startdate == vm.chartData[j].data[k].cat) {
+              vm.alldata[j].data[k].y += tb[i].Duration_s_ / 3600;
+            }
+          }
+        }
+      }
       create_downtime_chart();
     }
 
@@ -135,13 +160,15 @@ define([], function () {
         xAxis: { type: 'category', categories: vm.categ },
         yAxis: {
           title: {
-            text: "Idő"
+            text: "Idő (óra)"
           }
         },
         title: { text: 'Állásidő eloszlás' },
         series: vm.chData
       };
     }
+
+    var refresall = setInterval(loadall, 60 * 60 * 1000);
     activate();
 
     function activate() {
