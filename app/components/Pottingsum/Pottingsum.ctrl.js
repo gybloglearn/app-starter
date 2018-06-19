56a3fff476542493a20c407f0932a1b904914db8
Albano <503045800@logon.ds.ge.com>
2018-06-19 10:36:16 +0200

    redgreen

--- app/components/Pottingsum/Pottingsum.ctrl.js
+++ app/components/Pottingsum/Pottingsum.ctrl.js
@@ -12,7 +12,7 @@ define([], function () {
     vm.startdate = $filter('date')(new Date().getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
     vm.enddate = $filter('date')(new Date().getTime() + (24 * 3600 * 1000), 'yyyy-MM-dd');
     vm.hely = ["Potting be", "Gel Prep Also F", "Uret Prep Also F", "Esztetika Also F", "Forgatas", "Uret Prep Felso F", "Esztetika Felso F", "Potting ki"];
-    vm.categories=["Száradási időre várás szárítóban","Száradási időre várás forgatáson","Száradási időre várás esztétikán","Uretánkötési probléma forgatásnál","Egyéb"];
+    vm.categories = ["Száradási időre várás szárítóban", "Száradási időre várás forgatáson", "Száradási időre várás esztétikán", "Uretánkötési probléma forgatásnál", "Egyéb"];
     vm.load = load;
     vm.saveinfo = saveinfo;
     vm.beallit = beallit;
@@ -55,12 +55,42 @@ define([], function () {
           vm.data[i].PT_OUT = new Date(vm.data[i].PT_OUT).getTime();
           populate(vm.data[i].PT_OUT, 7);
 
+          //különbség1<130
+          if (vm.data[i].PT_URET_PREP_A_DT > 0 && vm.data[i].PT_POST_URET_A_DT > 0) {
+            vm.data[i].difference_PT_URET_PREP_A_DT_PT_POST_URET_A_DT = (vm.data[i].PT_POST_URET_A_DT - vm.data[i].PT_URET_PREP_A_DT) / (60 * 1000);
+          }
+          else {
+            vm.data[i].difference_PT_URET_PREP_A_DT_PT_POST_URET_A_DT = 0;
+          }
+          //különbség2<150
+          if (vm.data[i].PT_POST_URET_A_DT > 0 && vm.data[i].PT_ROT_DT) {
+            vm.data[i].difference_PT_POST_URET_A_DT_PT_ROT_DT = (vm.data[i].PT_ROT_DT - vm.data[i].PT_POST_URET_A_DT) / (60 * 1000);
+          }
+          else {
+            vm.data[i].difference_PT_POST_URET_A_DT_PT_ROT_DT = 0;
+          }
+          //különbség3<130
+          if (vm.data[i].PT_URET_PREP_F_DT > 0 && vm.data[i].PT_POST_URET_F_DT > 0) {
+            vm.data[i].difference_PT_URET_PREP_F_DT_PT_POST_URET_F_DT = (vm.data[i].PT_POST_URET_F_DT - vm.data[i].PT_URET_PREP_F_DT) / (60 * 1000);
+          }
+          else {
+            vm.data[i].difference_PT_URET_PREP_F_DT_PT_POST_URET_F_DT = 0;
+          }
+          //különbség4<400
+          if (vm.data[i].PT_POST_URET_F_DT > 0 && vm.data[i].PT_OUT > 0) {
+            vm.data[i].difference_PT_POST_URET_F_DT_PT_OUT = (vm.data[i].PT_OUT - vm.data[i].PT_POST_URET_F_DT) / (60 * 1000);
+          }
+          else {
+            vm.data[i].difference_PT_POST_URET_F_DT_PT_OUT = 0;
+          }
+
         }
 
         loadarchivefile();
         //chartize();
 
         vm.dis = false;
+        console.log(vm.data);
 
       });
     }
@@ -104,7 +134,7 @@ define([], function () {
               rsp.data[j].category = k;
               vm.dryarchivedata.push(rsp.data[j]);
             }
-            else if (vm.mch == "Potting3" && rsp.data[j].machinename == "Drying2" && pnsinpott2.indexOf(rsp.data[j].modul_a.substr(2,7)) == -1 && rsp.data[j].Time_to_Go < 6) {
+            else if (vm.mch == "Potting3" && rsp.data[j].machinename == "Drying2" && pnsinpott2.indexOf(rsp.data[j].modul_a.substr(2, 7)) == -1 && rsp.data[j].Time_to_Go < 6) {
               rsp.data[j].category = k;
               vm.dryarchivedata.push(rsp.data[j]);
             }
@@ -176,11 +206,11 @@ define([], function () {
         { visible: false, name: 'Uret PREP F', color: 'rgb(50,150,200)', data: [] },
         { visible: false, name: 'Esztetika F', color: 'rgb(250,100,100)', data: [] },
         { visible: false, name: 'Potting Ki', color: 'rgb(250,200,150)', data: [] },
-        { name: 'Cél', color:'rgb(255,0,0)', type: 'line', data: [] }
+        { name: 'Cél', color: 'rgb(255,0,0)', type: 'line', data: [] }
       ];
       for (var k = 1; k < 25; k++) {
         vm.cats.push($filter('date')(vm.kezdo + k * 60 * 60 * 1000, "MMdd HH"));
-        for (var i = 0; i < vm.chartData.length-1; i++) {
+        for (var i = 0; i < vm.chartData.length - 1; i++) {
           vm.chartData[i].data.push({ cat: $filter('date')(vm.kezdo + k * 60 * 60 * 1000, "MMdd HH"), y: 0, sh: 0 });
         }
       }
@@ -189,77 +219,77 @@ define([], function () {
     function updateline(arr) {
       vm.linenumbers = [];
 
-      var number1=0;
-      var number2=0;
-      var number3=0;
-      var number4=0;
+      var number1 = 0;
+      var number2 = 0;
+      var number3 = 0;
+      var number4 = 0;
 
       for (var i = 0; i < 24; i++) {
         vm.linenumbers[i] = 0;
       }
 
-      for(var i=0;i<arr.length;i++){
-        if(arr[i].category==1){
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i].category == 1) {
           number1++;
         }
-        if(arr[i].category==2){
+        if (arr[i].category == 2) {
           number2++;
         }
-        if(arr[i].category==3){
+        if (arr[i].category == 3) {
           number3++;
         }
-        if(arr[i].category==4){
+        if (arr[i].category == 4) {
           number4++;
         }
       }
-      if(vm.mch=="Potting4"){
-        if(number1>24){
-          number1=24
+      if (vm.mch == "Potting4") {
+        if (number1 > 24) {
+          number1 = 24
         }
-        if(number2>24){
-          number2=24
+        if (number2 > 24) {
+          number2 = 24
         }
-        if(number3>24){
-          number3=24
+        if (number3 > 24) {
+          number3 = 24
         }
-        if(number4>24){
-          number4=24
-        }
-      }
-      if(vm.mch=="Potting3"){
-        if(number1>18){
-          number1=18
-        }
-        if(number2>18){
-          number2=18
-        }
-        if(number3>18){
-          number3=18
-        }
-        if(number4>18){
-          number4=18
+        if (number4 > 24) {
+          number4 = 24
         }
       }
-      for(var i=0;i<24;i++){
-        if(i>=0 && i<6){
+      if (vm.mch == "Potting3") {
+        if (number1 > 18) {
+          number1 = 18
+        }
+        if (number2 > 18) {
+          number2 = 18
+        }
+        if (number3 > 18) {
+          number3 = 18
+        }
+        if (number4 > 18) {
+          number4 = 18
+        }
+      }
+      for (var i = 0; i < 24; i++) {
+        if (i >= 0 && i < 6) {
           //vm.linenumbers[i]=Math.round(number1/6)*2;
-          vm.linenumbers[i]=parseFloat(number1/6*2);
+          vm.linenumbers[i] = parseFloat(number1 / 6 * 2);
         }
-        else if(i>=6 && i<12){
+        else if (i >= 6 && i < 12) {
           //vm.linenumbers[i]=Math.round(number2/6)*2;
-          vm.linenumbers[i]=parseFloat(number2/6*2);
+          vm.linenumbers[i] = parseFloat(number2 / 6 * 2);
         }
-        else if(i>=12 && i<18){
+        else if (i >= 12 && i < 18) {
           //vm.linenumbers[i]=Math.round(number3/6)*2;
-          vm.linenumbers[i]=parseFloat(number3/6*2);
+          vm.linenumbers[i] = parseFloat(number3 / 6 * 2);
         }
-        else{
+        else {
           //vm.linenumbers[i]=Math.round(number4/6)*2;
-          vm.linenumbers[i]=parseFloat(number4/6*2);
+          vm.linenumbers[i] = parseFloat(number4 / 6 * 2);
         }
       }
       //console.log(vm.linenumbers);
-      vm.chartData[vm.chartData.length-1].data = vm.linenumbers;
+      vm.chartData[vm.chartData.length - 1].data = vm.linenumbers;
       chartize();
     }
 
@@ -276,7 +306,7 @@ define([], function () {
 
       vm.createinfodata = {};
       vm.actplace = "";
-      vm.cat="";
+      vm.cat = "";
       vm.descriptioninfo = "";
       vm.startinfo = new Date().getFullYear() + "-" + str.substring(0, 2) + "-" + str.substring(2, 7) + ":" + "00";
       vm.endinfo = new Date().getFullYear() + "-" + str.substring(0, 2) + "-" + str.substring(2, 7) + ":" + "00";
@@ -293,7 +323,7 @@ define([], function () {
       vm.createinfodata.time = vm.timeinfo = (new Date(vm.endinfo).getTime() - new Date(vm.startinfo).getTime()) / 60000;
       vm.createinfodata.pottingid = vm.mch;
       vm.createinfodata.place = vm.actplace;
-      vm.createinfodata.category=vm.cat;
+      vm.createinfodata.category = vm.cat;
       vm.createinfodata.description = vm.descriptioninfo;
       vm.createinfodata.opid = $rootScope.user.username;
       vm.createinfodata.opname = $rootScope.user.displayname
