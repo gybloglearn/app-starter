5e2fa4c635f5ffd30082c97c35daefed897f4f61
Albano <503045800@logon.ds.ge.com>
2018-06-28 13:02:44 +0200

    change_PHP

--- app/components/services/sscrap.service.js
+++ app/components/services/sscrap.service.js
@@ -3,6 +3,7 @@ define([], function () {
 
   function Service($http) {
     var service = {
+      getpartnumber: getpartnumber,
       getsheet: getsheet,
       getscrap: getscrap
     };
@@ -11,14 +12,28 @@ define([], function () {
 
     ///////////
 
-    function getsheet(start, end, mch) {
+    /*function getsheet(start, end, mch) {
       var req = {
         method: 'GET',
         url: 'app/components/PHP/ZW500_SM.php?startdate=' + start + '&enddate=' + end + '&report_id=' + mch
       };
       return $http(req);
+    }*/
+    function getpartnumber() {
+      var req = {
+        method: 'GET',
+        url: 'http://3.228.180.13/modulapi/mods'
+      };
+      return $http(req);
     }
-    function getscrap(st,ed) {
+    function getsheet(start, end) {
+      var req = {
+        method: 'GET',
+        url: 'app/components/PHP/Smtable.php?startdate=' + start + '&enddate=' + end
+      };
+      return $http(req);
+    }
+    function getscrap(st, ed) {
       var req = {
         method: 'GET',
         url: 'app/components/PHP/sm.scraps.php?startdate=' + st + '&enddate=' + ed
