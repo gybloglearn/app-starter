c39aaa3bce9353da8a342e04bb62eef318a3ea45
Albano <503045800@logon.ds.ge.com>
2018-07-02 09:51:39 +0200

    mod

--- app/components/uf/uf.ctrl.js
+++ app/components/uf/uf.ctrl.js
@@ -99,7 +99,6 @@ define([], function () {
 								case '04': targ = 60; break;
 								case '05': targ = 70; break;
 								case '06': targ = 80; break;
-								case '07': targ = 80; break;
 								default: targ = 60; break;
 							}
 							vm.target.push({ name: k[ki].SPL_end, y: targ });
