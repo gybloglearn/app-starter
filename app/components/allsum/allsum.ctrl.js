7f0118350bb5a7fa48f5a1332f162acc5ff57f4f
Albano <503045800@logon.ds.ge.com>
2019-02-05 13:40:21 +0100

    add_allsum

--- app/components/allsum/allsum.ctrl.js
+++ app/components/allsum/allsum.ctrl.js
@@ -2,11 +2,11 @@ define([], function () {
   'use strict';
   function Controller(allsumService, $cookies, $state, $rootScope, $filter) {
     var vm = this;
-    vm.startdate = $filter('date')(new Date().getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
-    vm.enddate = $filter('date')(new Date().getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
+    vm.date = $filter('date')(new Date().getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
+    vm.edate = $filter('date')(new Date().getTime() - (24 * 3600 * 1000), 'yyyy-MM-dd');
     vm.sheetmakers = ["SM1", "SM2", "SM4", "SM5", "SM6", "SM7", "SM8", "SM9"];
     vm.sm = [];
-    vm.createdates = createdates;
+    vm.callsm = callsm;
 
     function loadPartnumbers() {
       vm.partnumbers = [];
@@ -16,22 +16,9 @@ define([], function () {
       });
     }
 
-    function createdates() {
-      vm.loaddata = true;
-      vm.dates = [];
-      vm.days = [];
-      var differencedate = 0;
-      differencedate = (new Date(vm.enddate).getTime() - new Date(vm.startdate).getTime()) / (24 * 3600 * 1000);
-      for (var i = 0; i <= differencedate; i++) {
-        vm.dates[i] = $filter('date')(new Date(vm.enddate).getTime() - ((differencedate - i) * 24 * 3600 * 1000), 'yyyyMMdd');
-        vm.days.push({
-          date: vm.dates[i], joaeq: 0, ttlaeq: 0, jolap: 0, ttllap: 0, lapselejt: 0, terv: 0, szer: 0, musz: 0, ttlido: 0
-        });
-      }
-      callsm();
-    }
-
     function callsm() {
+      vm.loaddata = true;
+      vm.sm = [];
       for (var i = 0; i < vm.sheetmakers.length; i++) {
         vm.sm[i] = {};
         vm.sm[i].id = vm.sheetmakers[i];
@@ -44,34 +31,111 @@ define([], function () {
         vm.sm[i].oaeq = 0;
         vm.sm[i].selejt = 0;
         vm.sm[i].saeq = 0;
+        vm.sm[i].misstime = 0;
+        vm.sm[i].timediff = 0;
+        vm.sm[i].alltime = 1440 * 60;
       }
-      vm.sm[i + 1] = {}
-      vm.sm[i + 1].id = "SMS";
-      vm.sm[i + 1].musz = 0;
-      vm.sm[i + 1].szerv = 0;
-      vm.sm[i + 1].terv = 0;
-      vm.sm[i + 1].jo = 0;
-      vm.sm[i + 1].jaeq = 0;
-      vm.sm[i + 1].ossz = 0;
-      vm.sm[i + 1].oaeq = 0;
-      vm.sm[i + 1].selejt = 0;
-      vm.sm[i + 1].saeq = 0;
+      vm.sm[i] = {}
+      vm.sm[i].id = "SMS";
+      vm.sm[i].musz = 0;
+      vm.sm[i].szerv = 0;
+      vm.sm[i].terv = 0;
+      vm.sm[i].jo = 0;
+      vm.sm[i].jaeq = 0;
+      vm.sm[i].ossz = 0;
+      vm.sm[i].oaeq = 0;
+      vm.sm[i].selejt = 0;
+      vm.sm[i].saeq = 0;
+      vm.sm[i].misstime = 0;
+      vm.sm[i].timediff = 0;
+      vm.sm[i].alltime = vm.sheetmakers.length * 1440 * 60;
 
-      console.log(vm.sm);
       load();
     }
 
     function load() {
-      vm.dt = [];
+      vm.tervezett=[];
+      vm.szervezesi=[];
+      vm.muszaki=[];
 
-      for (var i = 0; i < vm.sheetmakers.length; i++) {
-        allsumService.get(vm.startdate, vm.sheetmakers[i]).then(function (response) {
-          var d=response.data;
-          for(var j=0;j<d.length;j++){
-            console.log(d[j]);
+      angular.forEach(vm.sheetmakers, function (v, k) {
+        allsumService.get(vm.date, v).then(function (response) {
+          /*var ter=$filter('filter')(response.data, { Ev_Group: "Tervezett veszteseg" });
+          var szer=$filter('filter')(response.data, { Ev_Group: "Szervezesi veszteseg" });
+          var musz=$filter('filter')(response.data, { Ev_Group: "Muszaki technikai okok" });
+
+          console.log(ter);*/
+          for (var i = 0; i < response.data.length; i++) {
+            var tmb = response.data[i];
+            var diff = 0;
+            var k = i + 1;
+
+            if (k <= response.data.length - 1) {
+              diff = (response.data[k].timestamp - response.data[i].timestamp) / 1000;
+            }
+
+            for (var j = 0; j < vm.sheetmakers.length; j++) {
+              if (v == vm.sheetmakers[j] && tmb.Ev_Group == "Tervezett veszteseg") {
+                vm.sm[j].terv += tmb.Event_time;
+                vm.sm[vm.sheetmakers.length].terv += tmb.Event_time;
+              }
+              else if (v == vm.sheetmakers[j] && tmb.Ev_Group == "Szervezesi veszteseg") {
+                vm.sm[j].szerv += tmb.Event_time;
+                vm.sm[vm.sheetmakers.length].szerv += tmb.Event_time;
+              }
+              else if (v == vm.sheetmakers[j] && tmb.Ev_Group == "Muszaki technikai okok") {
+                vm.sm[j].musz += tmb.Event_time;
+                vm.sm[vm.sheetmakers.length].musz += tmb.Event_time;
+              }
+              else if (v == vm.sheetmakers[j]) {
+                vm.sm[j].misstime += diff;
+                vm.sm[j].timediff += diff - tmb.Event_time;
+                vm.sm[vm.sheetmakers.length].misstime += diff;
+                vm.sm[vm.sheetmakers.length].timediff += diff - tmb.Event_time;
+              }
+            }
           }
         });
-      }
+      });
+      loadsm();
+    }
+
+    function loadsm() {
+      allsumService.getsheet(vm.date, $filter('date')(new Date(vm.date).getTime() + 24 * 60 * 60 * 1000, "yyyy-MM-dd")).then(function (response) {
+        var d = response.data;
+
+        for (var i = 0; i < vm.partnumbers.length; i++) {
+          for (var j = 0; j < d.length; j++) {
+            if (vm.partnumbers[i].id == d[j].type) {
+              d[j].Goodsheets = d[j].Totalsheets - d[j].ScrapSheets;
+              d[j].Goodaeq = ((d[j].Totalsheets - d[j].ScrapSheets) / vm.partnumbers[i].sheets) * vm.partnumbers[i].aeq;
+              d[j].Scrapaeq = (d[j].ScrapSheets / vm.partnumbers[i].sheets) * vm.partnumbers[i].aeq;
+              d[j].Totalaeq = (d[j].Totalsheets / vm.partnumbers[i].sheets) * vm.partnumbers[i].aeq;
+              d[j].shortname = d[j].MachineName[0] + d[j].MachineName[5] + d[j].MachineName[10];
+            }
+          }
+        }
+
+        for (var j = 0; j < d.length; j++) {
+          for (var k = 0; k < vm.sheetmakers.length; k++) {
+            if (d[j].shortname == vm.sm[k].id) {
+              vm.sm[k].jo += d[j].Goodsheets * 1;
+              vm.sm[k].jaeq += d[j].Goodaeq * 1;
+              vm.sm[k].selejt += d[j].ScrapSheets * 1;
+              vm.sm[k].saeq += d[j].Scrapaeq * 1;
+              vm.sm[k].ossz += d[j].Totalsheets * 1;
+              vm.sm[k].oaeq += d[j].Totalaeq * 1;
+              vm.sm[vm.sheetmakers.length].jo += d[j].Goodsheets * 1;
+              vm.sm[vm.sheetmakers.length].jaeq += d[j].Goodaeq * 1;
+              vm.sm[vm.sheetmakers.length].selejt += d[j].ScrapSheets * 1;
+              vm.sm[vm.sheetmakers.length].saeq += d[j].Scrapaeq * 1;
+              vm.sm[vm.sheetmakers.length].ossz += d[j].Totalsheets * 1;
+              vm.sm[vm.sheetmakers.length].oaeq += d[j].Totalaeq * 1;
+            }
+          }
+        }
+        console.log(vm.sm);
+      });
     }
 
     activate();
--- app/components/misstime/misstime.ctrl.js
+++ app/components/misstime/misstime.ctrl.js
@@ -39,56 +39,6 @@ define([], function () {
       }
     }
 
-      /*if (vm.sm != "") {
-        smdataService.get(vm.datum, vm.sm).then(function (response) {
-          vm.data = response.data;
-
-          for (var i = 0; i < vm.data.length - 1; i++) {
-            var j = i + 1
-            var diff = (vm.data[j].timestamp - vm.data[i].timestamp);
-            vm.misst += diff / 1000 - vm.data[i].Event_time;
-
-            var obj = {
-              type: vm.data[i].Event_type,
-              start: vm.data[i].timestamp,
-              nextstart: vm.data[j].timestamp,
-              time: vm.data[i].Event_time,
-              difference: diff / 1000,
-              timediff: diff / 1000 - vm.data[i].Event_time
-            }
-            vm.missdata.push(obj);
-          }
-          vm.smloading = false;
-        });
-      }
-      else{
-        vm.smloading = true;
-        for(var i=0;i<vm.sheetmakers.length;i++){
-          smdataService.get(vm.datum, vm.sheetmakers[i]).then(function (response) {
-            vm.data = response.data;
-  
-            for (var j = 0; j < vm.data.length - 1; j++) {
-              var k = j + 1
-              var diff = (vm.data[k].timestamp - vm.data[j].timestamp);
-              vm.misst += diff / 1000 - vm.data[j].Event_time;
-  
-              var obj = {
-                type: vm.data[j].Event_type,
-                start: vm.data[j].timestamp,
-                nextstart: vm.data[k].timestamp,
-                time: vm.data[j].Event_time,
-                difference: diff / 1000,
-                timediff: diff / 1000 - vm.data[j].Event_time
-              }
-              vm.missdata.push(obj);
-            }
-            vm.smloading = false;
-          });
-        }
-      }
-    }*/
-    
-
     activate();
 
     function activate() {
