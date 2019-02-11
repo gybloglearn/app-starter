1777f6d7df33dfe76e392c6ce4d1ef458f3c64d1
Albano <503045800@logon.ds.ge.com>
2019-02-11 10:28:21 +0100

    jav

--- app/components/PHP/Pottinginfosave/index.php
+++ app/components/PHP/Pottinginfosave/index.php
@@ -67,7 +67,7 @@ class info{
         //$mapper=new DB\SQL\Mapper($db,'info');
         $mapper=new DB\SQL\Mapper($db,'info1000');
         $info=$mapper->find(Array('id=?',$params['id']));
-        echo $info;
+        echo $info[0];
         $info[0]->erase();
         echo "OK";
         @unlink($mapper);
